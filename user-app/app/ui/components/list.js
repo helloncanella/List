95c1b04b4c6c533eefd6ee3efca47c1e709a1fee
Hellon Canella <helloncanella@gmail.com>
2017-02-08 21:05:55 -0200

    fine

---
+++ user-app/app/ui/components/list.js
@@ -0,0 +1,89 @@
+import React, { Component, PropTypes } from 'react'
+import { StyleSheet, Text, View, ListView, Image, TouchableHighlight } from 'react-native'
+import {typography, color, grid, pressStyle} from '/ui/stylesheets/global.js'
+
+const rowHasChanged = (r1, r2) => r1 !== r2
+    , ds = new ListView.DataSource({ rowHasChanged })
+
+export default class List extends Component {
+
+    touchProps() {
+        return {
+            underlayColor:pressStyle.underlayColor,
+            activeOpacity: pressStyle.activeOpacity,
+            style: styles.container
+        } 
+    }
+
+    renderRow({primaryText, headerText = '', secondaryText = '', image, payload}) {
+
+        const {thumbnail, row, secondaryTextStyle, primaryTextStyle, headerStyle} = styles
+            , touchProps = this.touchProps()
+            , {onSelectItem} = this.props
+            , {big} = typography
+
+        return (
+            <TouchableHighlight {...touchProps} onPress={() => onSelectItem(payload)}>
+                <View style={row}>
+                    {image ? <Image style={thumbnail} source={{ uri: image }} /> : null}
+                    <View>
+                        {headerText ? <Text style={headerStyle}>{headerText}</Text> : null}
+                        <Text style={primaryTextStyle}>{primaryText}</Text>
+                        {secondaryText ? <Text style={secondaryTextStyle}>{secondaryText}</Text> : null}
+                    </View>
+                </View>
+            </TouchableHighlight>
+        )
+    }
+
+    render() {
+        const {data} = this.props
+
+        return (
+            <ListView
+                style={styles.container}
+                dataSource={ds.cloneWithRows(data)}
+                renderRow={this.renderRow.bind(this)}
+                enableEmptySections={true}
+                />
+        )
+    }
+}
+
+List.propTypes = {
+    onSelectItem: PropTypes.func.isRequired,
+    data: PropTypes.array.isRequired
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    row: {
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: color.borderLine,
+        padding: grid.padding,
+        alignItems: 'center'
+    },
+    thumbnail: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        marginRight: 10
+    },
+    headerStyle: {
+        marginBottom: 2,
+        fontSize: typography.small,
+        color: color.silentText
+    },
+    secondaryTextStyle: {
+        fontSize: typography.small,
+        color: color.silentText
+    },
+    primaryTextStyle: {
+        fontSize: typography.normal,
+        color: color.primary,
+        marginBottom: 3
+    }
+});
--- user-app/app/ui/components/notifications.js
+++ user-app/app/ui/components/notifications.js
@@ -1,20 +1,58 @@
 import React, { Component, PropTypes } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { database } from '/library/database'
+import List from '/ui/components/list.js'
 
-export default class Notifications extends Component {
-  render() {
-    const {container} = styles
-    return (
-      <View style={container}></View>
-    );
-  }
+export class Notifications extends Component {
+	
+	formatedTime(){
+		return '1 min. atrás'
+	}
+
+	onSelectNotification(notificationId) {
+
+	}
+
+	formatedNotifications(){
+		return this.props.notification.map((notification)=>{
+
+			const {_id:notificationId, message, sender, timeStamp} = notification
+			
+			return {
+				image: notification.sender.image, 
+				primaryText: message, 
+				secondaryText, 
+				payLoad: notification._id
+			}
+
+		})
+	}
+
+	render() {
+		const {container} = styles
+			, notifications = this.formatedNotifications()
+			, onSelectItem = this.onSelectNotification.bind(this)
+
+		return (
+			<View style={container}>
+				<List data={}></List>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1
-  }
+	container: {
+		flex: 1
+	}
 });
 
-
 Notifications.propTypes = {}
+
+export default database.createContainer((props) => {
+	return {
+		notifications: [
+			{}
+		]
+	}
+})
